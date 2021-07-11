4816ad492b2c286322b77453e88033f1991135ce
Wasif Baliyan <w.baliyan@gmail.com>
2021-07-11 18:57:17 +0530

    add dist folder in webpack prod build

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     // filename: "app.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "public/dist"),
   },
   module: {
     rules: [
