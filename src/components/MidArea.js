132db150b0df13e18952b9011ad53df0b525d6f2
Wasif Baliyan <w.baliyan@gmail.com>
2021-07-11 18:46:43 +0530

    first commit

--- src/components/MidArea.js
+++ src/components/MidArea.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function MidArea() {
-  return <div className="flex-1 h-full overflow-auto">{"mid area"} </div>;
+export default function MidArea({ handleClick, setTasks, tasks }) {
+  const drop = (e) => {
+    e.preventDefault();
+    const card_id = e.dataTransfer.getData("card_id");
+    const cardCopy = document.getElementById(card_id).cloneNode(true);
+    setTasks([...tasks, { name: cardCopy.id }]);
+    cardCopy.addEventListener("click", () => handleClick(cardCopy.id));
+    cardCopy.id = "new_" + cardCopy.id;
+    e.target.appendChild(cardCopy);
+  };
+  const dragOver = (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  };
+  return (
+    <div
+      id="target"
+      onDrop={drop}
+      onDragOver={dragOver}
+      className="flex-1 h-full overflow-auto mx-16"
+    ></div>
+  );
 }
